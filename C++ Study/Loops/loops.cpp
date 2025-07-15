8dc3274974116b27f988eccdc242b48676607edc
Noriel Achero <acheronorielr@gmail.com>
2025-07-15 20:51:08 +0800

    123123

---
+++ C++ Study/Loops/loops.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // for (int i = 1; i < 10; i++)
+    // {
+
+    //     cout << "Value f i is : " << i << endl;
+    // }
+
+    // int a = 10;
+
+    // while (a <= 30)
+    // {
+    //     cout << "The Value of a is " << a << endl;
+    //     a++;
+    // }
+
+    int a = 1;
+
+    do
+    {
+        cout << "Value of a is " << a << endl;
+        a++;
+    } while (a < 10);
+
+    return 0;
+}
